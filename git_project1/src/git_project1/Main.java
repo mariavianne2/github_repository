5a5f439cf9d1c40dd1766bc6e10e12132bd9f2fb
ghdo <ghdo@T500>
2017-05-18 14:27:00 +0900

    원격저장소로 로컬저장소 생성 그리고 프로젝트 생성

--- git_project1/src/git_project1/Main.java
+++ git_project1/src/git_project1/Main.java
@@ -10,6 +10,7 @@ public class Main {
 		System.out.println("첫 번째");
 		System.out.println("branch1에서 변경");
 		System.out.println("원격저장소 연결 후 master에서 변경");
+		System.out.println("원격저장소로 로컬저장소 생성 그리고 프로젝트 생성");
 	}
 
 }
