bcd2f30a4e9c21575360cfdc1aff6a9eea37a91c
ghdo <ghdo@T500>
2017-05-18 13:21:47 +0900

    원격저장소 연결 후 master에서 변경

--- git_project1/src/git_project1/Main.java
+++ git_project1/src/git_project1/Main.java
@@ -9,6 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("첫 번째");
 		System.out.println("branch1에서 변경");
+		System.out.println("원격저장소 연결 후 master에서 변경");
 	}
 
 }
