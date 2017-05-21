a34f356f97c5261fd1363414d7b28b30e429095f
ghdo <ghdo@T500>
2017-05-21 16:03:53 +0900

    lll

--- git_project1/src/git_project1/Main.java
+++ git_project1/src/git_project1/Main.java
@@ -7,11 +7,7 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("泥� 踰덉㎏");
-		System.out.println("branch1�뿉�꽌 蹂�寃�");
-		System.out.println("�썝寃⑹��옣�냼 �뿰寃� �썑 master�뿉�꽌 蹂�寃�");
-		System.out.println("�썝寃⑹��옣�냼濡� 濡쒖뺄���옣�냼 �깮�꽦 洹몃━怨� �봽濡쒖젥�듃 �깮�꽦");
-		System.out.println("again remote i"); 
+		System.out.println("again local"); 
 	}
 
 }
