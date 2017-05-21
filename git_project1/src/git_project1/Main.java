2bf9780dcb368a7837861b180dd2b59eeedb0bb4
ghdo <ghdo@T500>
2017-05-21 16:01:54 +0900

    pp

--- git_project1/src/git_project1/Main.java
+++ git_project1/src/git_project1/Main.java
@@ -7,11 +7,11 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("첫 번째");
-		System.out.println("branch1에서 변경");
-		System.out.println("원격저장소 연결 후 master에서 변경");
-		System.out.println("원격저장소로 로컬저장소 생성 그리고 프로젝트 생성");
-		System.out.println("again remote"); 
+		System.out.println("泥� 踰덉㎏");
+		System.out.println("branch1�뿉�꽌 蹂�寃�");
+		System.out.println("�썝寃⑹��옣�냼 �뿰寃� �썑 master�뿉�꽌 蹂�寃�");
+		System.out.println("�썝寃⑹��옣�냼濡� 濡쒖뺄���옣�냼 �깮�꽦 洹몃━怨� �봽濡쒖젥�듃 �깮�꽦");
+		System.out.println("again remote i"); 
 	}
 
 }
